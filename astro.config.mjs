967a48af651025ac088ad679c29cb96fb71d8853
BeardVis <BeardVis@outlook.com>
2026-07-06 13:33:21 +0300

    Add main layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,35 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
+
+import tailwindcss from '@tailwindcss/vite';
+
+import icon from "astro-icon";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()]
+  },
+
+  fonts: [{
+    provider: fontProviders.google(),
+    name: "EB Garamond",
+    cssVariable: "--font-eb-garamond",
+  },
+  {
+    provider: fontProviders.google(),
+    name: "Inter",
+    cssVariable: "--font-inter",
+  },
+  {
+    provider: fontProviders.googleicons(),
+    name: "Material Symbols Outlined",
+    cssVariable: "--font-material-symbols",
+  }],
+
+  integrations: [icon({
+      include: {
+        mdi: ["instagram", "facebook", "youtube"],
+      },
+    })]
+});
